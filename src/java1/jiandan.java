cbb6b5ee313dc149fea025818c337e367a6050e9
yuhaiboyy <1556636208@qq.com>
2016-09-02 12:01:26 +0800

    enen

---
+++ src/java1/jiandan.java
@@ -0,0 +1,46 @@
+package java1;
+
+public class jiandan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int num=10;
+		num=num*2;
+		  System.out.println(num);
+		int max=Integer.MAX_VALUE;
+		int min=Integer.MIN_VALUE;
+		System.out.println(max+1L);
+		System.out.println(min-1L);
+		System.out.println((long)max+1);
+		long num=100L;
+		int temp=(int) num;
+		System.out.println(temp);
+		long num=2147483649L;
+		long temp=num;
+		System.out.println(temp);
+				byte b=(byte)1000L;
+				System.out.println(b);
+				float num=10.22F;
+				num=num*2;
+				  System.out.println(num);
+				  double x=10;
+				  float y=4F;
+				  System.out.println(x/y);
+				char c='A'	;
+				char d='¿Ó';
+				System.out.println(c);
+				System.out.println(d);
+				char c='a';
+				int num=c;
+				System.out.println(num);*/
+				char c='a';
+				int num=c;
+				num=num-32;
+				System.out.println((char)num);
+				char d='1';
+				int f=49;
+				System.out.println((int)d);
+				System.out.println(d==f);
+	}
+
+}
